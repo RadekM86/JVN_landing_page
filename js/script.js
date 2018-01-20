21f3fa758fe9733f66e802d8c4c97d81c38d1516
RadekM86 <byrm.design@gmail.com>
2018-01-20 15:50:23 +0100

    Added animation to logo and background

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ var darkMagenta = "#FF1BFF";
 var greenBlack = "#001001";
 var neonGreen = "#00CC9C";
 var neonLime = "#39ff14"
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+
+        
+    })
